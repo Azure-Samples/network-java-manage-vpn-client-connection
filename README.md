7408403444a700f6f611d0c3ec3772fc3c804c68
Azure-Fluent <azfluent@microsoft.com>
2020-10-26 13:13:22 +0800

    Release azure-resourcemanager 2.0.0

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ languages:
 products:
 - azure
 extensions:
-  services: virtual-network
+  services: Network
   platforms: java
 ---
 
@@ -13,26 +13,26 @@ extensions:
 
 
   Azure Network sample for managing virtual network gateway.
-   - Create a virtual network with subnets
-   - Create virtual network gateway
-   - Update virtual network gateway with Point-to-Site connection configuration
-   - Generate and download VPN client configuration package. Now it can be used to create VPN connection to Azure.
-   - Revoke a client certificate
- 
-   Please note: in order to run this sample, you need to have:
-    - pre-generated root certificate and public key exported to $CERT_PATH file
-       For more details please see https://docs.microsoft.com/en-us/azure/vpn-gateway/vpn-gateway-certificates-point-to-site for PowerShell instructions
-       and https://docs.microsoft.com/en-us/azure/vpn-gateway/vpn-gateway-certificates-point-to-site-makecert for Makecert instructions.
-    - client certificate generated for this root certificate installed on your machine.
-       Please see: https://docs.microsoft.com/en-us/azure/vpn-gateway/point-to-site-how-to-vpn-client-install-azure-cert
-    - thumbprint for client certificate saved to $CLIENT_CERT_THUMBPRINT
+  - Create a virtual network with subnets
+  - Create virtual network gateway
+  - Update virtual network gateway with Point-to-Site connection configuration
+  - Generate and download VPN client configuration package. Now it can be used to create VPN connection to Azure.
+  - Revoke a client certificate
+  <p>
+  Please note: in order to run this sample, you need to have:
+  - pre-generated root certificate and public key exported to $CERT_PATH file
+  For more details please see https://docs.microsoft.com/azure/vpn-gateway/vpn-gateway-certificates-point-to-site for PowerShell instructions
+  and https://docs.microsoft.com/azure/vpn-gateway/vpn-gateway-certificates-point-to-site-makecert for Makecert instructions.
+  - client certificate generated for this root certificate installed on your machine.
+  Please see: https://docs.microsoft.com/azure/vpn-gateway/point-to-site-how-to-vpn-client-install-azure-cert
+  - thumbprint for client certificate saved to $CLIENT_CERT_THUMBPRINT
  
 
 ## Running this Sample ##
 
 To run this sample:
 
-Set the environment variable `AZURE_AUTH_LOCATION` with the full path for an auth file. See [how to create an auth file](https://github.com/Azure/azure-libraries-for-java/blob/master/AUTH.md).
+See [DefaultAzureCredential](https://github.com/Azure/azure-sdk-for-java/tree/master/sdk/identity/azure-identity#defaultazurecredential) and prepare the authentication works best for you. For more details on authentication, please refer to [AUTH.md](https://github.com/Azure/azure-sdk-for-java/blob/master/sdk/resourcemanager/docs/AUTH.md).
 
     git clone https://github.com/Azure-Samples/network-java-manage-vpn-client-connection.git
 
@@ -42,9 +42,11 @@ Set the environment variable `AZURE_AUTH_LOCATION` with the full path for an aut
 
 ## More information ##
 
-[http://azure.com/java](http://azure.com/java)
+For general documentation as well as quickstarts on how to use Azure Management Libraries for Java, please see [here](https://aka.ms/azsdk/java/mgmt).
 
-If you don't have a Microsoft Azure subscription you can get a FREE trial account [here](http://go.microsoft.com/fwlink/?LinkId=330212)
+Start to develop applications with Java on Azure [here](http://azure.com/java).
+
+If you don't have a Microsoft Azure subscription you can get a FREE trial account [here](http://go.microsoft.com/fwlink/?LinkId=330212).
 
 ---
 
--- src/main/java/com/azure/resourcemanager/network/samples/ManageVpnGatewayPoint2SiteConnection.java
+++ src/main/java/com/azure/resourcemanager/network/samples/ManageVpnGatewayPoint2SiteConnection.java
@@ -1,59 +1,61 @@
-/**
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See License.txt in the project root for
- * license information.
- */
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 
-package com.microsoft.azure.management.network.samples;
+package com.azure.resourcemanager.network.samples;
 
-import com.microsoft.azure.management.Azure;
-import com.microsoft.azure.management.network.Network;
-import com.microsoft.azure.management.network.VirtualNetworkGateway;
-import com.microsoft.azure.management.network.VirtualNetworkGatewaySkuName;
-import com.microsoft.azure.management.resources.fluentcore.arm.Region;
-import com.microsoft.azure.management.resources.fluentcore.utils.SdkContext;
-import com.microsoft.azure.management.samples.Utils;
-import com.microsoft.rest.LogLevel;
+import com.azure.core.credential.TokenCredential;
+import com.azure.core.http.policy.HttpLogDetailLevel;
+import com.azure.core.management.AzureEnvironment;
+import com.azure.identity.DefaultAzureCredentialBuilder;
+import com.azure.resourcemanager.AzureResourceManager;
+import com.azure.resourcemanager.network.models.Network;
+import com.azure.resourcemanager.network.models.VirtualNetworkGateway;
+import com.azure.resourcemanager.network.models.VirtualNetworkGatewaySkuName;
+import com.azure.core.management.Region;
+import com.azure.core.management.profile.AzureProfile;
+import com.azure.resourcemanager.samples.Utils;
 
 import java.io.File;
+import java.io.IOException;
 
 /**
  * Azure Network sample for managing virtual network gateway.
- *  - Create a virtual network with subnets
- *  - Create virtual network gateway
- *  - Update virtual network gateway with Point-to-Site connection configuration
- *  - Generate and download VPN client configuration package. Now it can be used to create VPN connection to Azure.
- *  - Revoke a client certificate
- *
- *  Please note: in order to run this sample, you need to have:
- *   - pre-generated root certificate and public key exported to $CERT_PATH file
- *      For more details please see https://docs.microsoft.com/en-us/azure/vpn-gateway/vpn-gateway-certificates-point-to-site for PowerShell instructions
- *      and https://docs.microsoft.com/en-us/azure/vpn-gateway/vpn-gateway-certificates-point-to-site-makecert for Makecert instructions.
- *   - client certificate generated for this root certificate installed on your machine.
- *      Please see: https://docs.microsoft.com/en-us/azure/vpn-gateway/point-to-site-how-to-vpn-client-install-azure-cert
- *   - thumbprint for client certificate saved to $CLIENT_CERT_THUMBPRINT
+ * - Create a virtual network with subnets
+ * - Create virtual network gateway
+ * - Update virtual network gateway with Point-to-Site connection configuration
+ * - Generate and download VPN client configuration package. Now it can be used to create VPN connection to Azure.
+ * - Revoke a client certificate
+ * <p>
+ * Please note: in order to run this sample, you need to have:
+ * - pre-generated root certificate and public key exported to $CERT_PATH file
+ * For more details please see https://docs.microsoft.com/azure/vpn-gateway/vpn-gateway-certificates-point-to-site for PowerShell instructions
+ * and https://docs.microsoft.com/azure/vpn-gateway/vpn-gateway-certificates-point-to-site-makecert for Makecert instructions.
+ * - client certificate generated for this root certificate installed on your machine.
+ * Please see: https://docs.microsoft.com/azure/vpn-gateway/point-to-site-how-to-vpn-client-install-azure-cert
+ * - thumbprint for client certificate saved to $CLIENT_CERT_THUMBPRINT
  */
 
 public final class ManageVpnGatewayPoint2SiteConnection {
 
     /**
      * Main function which runs the actual sample.
-     * @param azure instance of the azure client
+     *
+     * @param azureResourceManager instance of the azure client
      * @return true if sample runs successfully
      */
-    public static boolean runSample(Azure azure) {
+    public static boolean runSample(AzureResourceManager azureResourceManager) throws IOException {
         final String certPath = System.getenv("CERT_PATH");
         final String clientCertThumbprint = System.getenv("CLIENT_CERT_THUMBPRINT");
         final Region region = Region.US_WEST2;
-        final String rgName = SdkContext.randomResourceName("rg", 20);
-        final String vnetName = SdkContext.randomResourceName("vnet", 20);
-        final String vpnGatewayName = SdkContext.randomResourceName("vngw", 20);
+        final String rgName = Utils.randomResourceName(azureResourceManager, "rg", 20);
+        final String vnetName = Utils.randomResourceName(azureResourceManager, "vnet", 20);
+        final String vpnGatewayName = Utils.randomResourceName(azureResourceManager, "vngw", 20);
 
         try {
             //============================================================
             // Create virtual network with address spaces 192.168.0.0/16 and 10.254.0.0/16 and 3 subnets
             System.out.println("Creating virtual network...");
-            Network network = azure.networks().define(vnetName)
+            Network network = azureResourceManager.networks().define(vnetName)
                     .withRegion(region)
                     .withNewResourceGroup(rgName)
                     .withAddressSpace("192.168.0.0/16")
@@ -69,7 +71,7 @@ public final class ManageVpnGatewayPoint2SiteConnection {
             //============================================================
             // Create virtual network gateway
             System.out.println("Creating virtual network gateway...");
-            VirtualNetworkGateway vngw1 = azure.virtualNetworkGateways().define(vpnGatewayName)
+            VirtualNetworkGateway vngw1 = azureResourceManager.virtualNetworkGateways().define(vpnGatewayName)
                     .withRegion(region)
                     .withExistingResourceGroup(rgName)
                     .withExistingNetwork(network)
@@ -108,25 +110,21 @@ public final class ManageVpnGatewayPoint2SiteConnection {
             System.out.println("Revoked client certificate");
 
             return true;
-        } catch (Exception e) {
-            System.err.println(e.getMessage());
-            e.printStackTrace();
         } finally {
             try {
                 System.out.println("Deleting Resource Group: " + rgName);
-                azure.resourceGroups().beginDeleteByName(rgName);
+                azureResourceManager.resourceGroups().beginDeleteByName(rgName);
             } catch (NullPointerException npe) {
                 System.out.println("Did not create any resources in Azure. No clean up is necessary");
             } catch (Exception g) {
                 g.printStackTrace();
             }
         }
-
-        return false;
     }
 
     /**
      * Main entry point.
+     *
      * @param args the parameters
      */
     public static void main(String[] args) {
@@ -134,17 +132,21 @@ public final class ManageVpnGatewayPoint2SiteConnection {
             //=============================================================
             // Authenticate
 
-            final File credFile = new File(System.getenv("AZURE_AUTH_LOCATION"));
+            final AzureProfile profile = new AzureProfile(AzureEnvironment.AZURE);
+            final TokenCredential credential = new DefaultAzureCredentialBuilder()
+                .authorityHost(profile.getEnvironment().getActiveDirectoryEndpoint())
+                .build();
 
-            Azure azure = Azure.configure()
-                    .withLogLevel(LogLevel.BODY)
-                    .authenticate(credFile)
-                    .withDefaultSubscription();
+            AzureResourceManager azureResourceManager = AzureResourceManager
+                .configure()
+                .withLogLevel(HttpLogDetailLevel.BASIC)
+                .authenticate(credential, profile)
+                .withDefaultSubscription();
 
             // Print selected subscription
-            System.out.println("Selected subscription: " + azure.subscriptionId());
+            System.out.println("Selected subscription: " + azureResourceManager.subscriptionId());
 
-            runSample(azure);
+            runSample(azureResourceManager);
         } catch (Exception e) {
             System.out.println(e.getMessage());
             e.printStackTrace();
